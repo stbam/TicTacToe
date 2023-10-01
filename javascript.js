991c98ab01767817adf55fa004ed980e00bafa24
stbam <55661516+stbam@users.noreply.github.com>
2023-10-01 14:17:05 -0400

    Basic logic

--- javascript.js
+++ javascript.js
@@ -1,14 +1,75 @@
-const Gameboard =( ()=> {
-    myArray:[]
+const Gameboard = (() => {
+    let gameboard = ['', '', '', '', '', '', '', '', '']; //['0', '0', '0', '0', '0', '0', '0', '0', '0'];
+  const x=  document.querySelectorAll('.square');  
+    x.forEach((element,i)=>{
+        const uniqueId=i+1;
+        element.id=uniqueId;
+    });
+  //  console.log(x[8].innerHTML)
+  
+    
 
 
+    // Return an object with a method to get the gameboard
+    return {
+     
+        getGameboard: function () {
+            return gameboard;
+        },
+        getSquares:function(){
+            return x;
+        }
 
-})
+    };
+})();
 
-function player(name){
+const displayController= (()=>{
+    let playerone=1;
+    let playertwo=0;
+    let turn=0;
+
+    const x =Gameboard.getSquares();
+    x.forEach((element,index)=>{
+        
+
+        element.innerHTML= Gameboard.getGameboard()[index];
+        element.addEventListener('click',()=>{
+              if(Gameboard.getGameboard()[index]==='X' || Gameboard.getGameboard()[index]==='O'){
+                //  console.log(Gameboard.getGameboard()[index]);       
+              }
+              else{
+               // console.log("spot isnt taken");
+                if(turn%2==1){
+                    player1(index);
+                    turn++;
+                    console.log(turn);
+                }
+                else{
+                    player2(index);
+                    turn++;
+                    console.log(turn);
+                }  
+              }        
+          })
+    });
+})();
+
+const player1=(index)=>{
+    let gameboard = Gameboard.getGameboard();
+    gameboard[index]='X';
+    updateDisplay();
 
 };
 
-const game= {
-
+const player2=(index)=>{
+ let gameboard = Gameboard.getGameboard();
+    gameboard[index]='O';
+    updateDisplay();
+    
+};
+const updateDisplay=()=>{
+    const x = Gameboard.getSquares();
+    x.forEach((element,index)=>{
+        element.innerHTML=Gameboard.getGameboard()[index];
+    })
 }
